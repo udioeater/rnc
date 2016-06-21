e82394008baed54b81b1313c367725986b568eaa
Kristen Smith <ksmith2@pillartechnology.com>
2016-06-21 14:15:08 -0400

    passing tests

--- src/rnc.c
+++ src/rnc.c
@@ -1,16 +1,26 @@
 #include "rnc.h"
 #include <string.h>
 
+static const char ORDER[7] = "MDCLXVI";
+
 void add(const char *first, int flen, const char *second, int slen, char *dst, int maxlen)
 {
     *dst = 0;
     int dstlen = 0;
 
-    strncat(dst, first, flen);
-    dstlen += flen;
+    if (index(ORDER, *first) < index(ORDER, *second)) {
+        strncat(dst, first, flen);
+        dstlen += flen;
 
-    strncat(dst, second, slen);
-    dstlen += slen;
+        strncat(dst, second, slen);
+        dstlen += slen;
+    } else {
+        strncat(dst, second, slen);
+        dstlen += slen;
+
+        strncat(dst, first, flen);
+        dstlen += flen;
+    }
 
     if (0 == strncmp(dst+dstlen-4, "IIII", 4)) {
         char *idx = index(dst, 'I');
