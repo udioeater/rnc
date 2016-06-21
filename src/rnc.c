31e816f30fcb88d3b378f671a6949a848066d7d1
Kristen Smith <ksmith2@pillartechnology.com>
2016-06-21 13:55:25 -0400

    passing tests

--- src/rnc.c
+++ src/rnc.c
@@ -4,10 +4,17 @@
 void add(const char *first, int flen, const char *second, int slen, char *dst, int maxlen)
 {
     *dst = 0;
-    strncat(dst, first, flen);
-    strncat(dst, second, slen);
+    int dstlen = 0;
 
-    if (0 == strncmp(dst, "IIII", 4)) {
-        strncpy(dst, "IV", maxlen);
+    strncat(dst, first, flen);
+    dstlen += flen;
+
+    strncat(dst, second, slen);
+    dstlen += slen;
+
+    if (0 == strncmp(dst+dstlen-4, "IIII", 4)) {
+        char *idx = index(dst, 'I');
+        strncpy(dst+(idx - dst), "IV", 4);
+        dstlen -= 2;
     }
 }
