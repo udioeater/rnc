b3e195bff6b0ec26cd124147e921a139e91ab134
Kristen Smith <ksmith2@pillartechnology.com>
2016-06-21 22:19:48 -0400

    add test for V+XI=XVI, for good measure

--- test/test_rnc.c
+++ test/test_rnc.c
@@ -68,6 +68,17 @@ START_TEST (add_XI_plus_V_returns_XVI)
 }
 END_TEST
 
+START_TEST (add_V_plus_XI_returns_XVI)
+{
+    int maxlen = 10;
+    char answer[maxlen];
+
+    add("V", 1, "XI", 2, answer, maxlen);
+
+    ck_assert_str_eq("XVI", answer);
+}
+END_TEST
+
 Suite* rnc_suite(voi)
 {
     Suite *s;
@@ -82,6 +93,7 @@ Suite* rnc_suite(voi)
     tcase_add_test(tc_core, add_XIII_plus_I_returns_XIV);
     tcase_add_test(tc_core, add_I_plus_V_returns_VI);
     tcase_add_test(tc_core, add_XI_plus_V_returns_XVI);
+    tcase_add_test(tc_core, add_V_plus_XI_returns_XVI);
     suite_add_tcase(s, tc_core);
 
     return s;
