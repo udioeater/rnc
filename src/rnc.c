3932837fdf05e21a4ec77f8902f007f983ee9ef7
Kristen Smith <ksmith2@pillartechnology.com>
2016-06-22 11:34:22 -0400

    passing tests

--- src/rnc.c
+++ src/rnc.c
@@ -36,7 +36,11 @@ static int shrink(char *dst, int dstlen)
     int i = 0;
 
     for (i = 0; i < dstlen; i++) {
-        if (0 == strncmp(dst+i, "LL", 2)) {
+        if (0 == strncmp(dst+i, "CCCCC", 5)) {
+            strncat(tmp, "D", 1);
+            tmplen++;
+            i+=4;
+        } else if (0 == strncmp(dst+i, "LL", 2)) {
             strncat(tmp, "C", 1);
             tmplen++;
             i++;
