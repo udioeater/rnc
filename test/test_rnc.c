6c8b4f4911d0af499b542b1b21c21f64336cb8a3
Kristen Smith <ksmith2@pillartechnology.com>
2016-06-23 09:18:35 -0400

    add test for XC+X=C (FAILING)

--- test/test_rnc.c
+++ test/test_rnc.c
@@ -222,6 +222,17 @@ START_TEST (add_XL_plus_X_returns_L)
 }
 END_TEST
 
+START_TEST (add_XC_plus_X_returns_C)
+{
+    int maxlen = 10;
+    char answer[maxlen];
+
+    add("XC", 2, "X", 1, answer, maxlen);
+
+    ck_assert_str_eq("C", answer);
+}
+END_TEST
+
 Suite* rnc_suite(void)
 {
     Suite *s;
@@ -256,6 +267,7 @@ Suite* rnc_suite(void)
     tcase_add_test(shorthand, add_XIV_plus_I_returns_XV);
     tcase_add_test(shorthand, add_V_plus_CIX_returns_CXIV);
     tcase_add_test(shorthand, add_XL_plus_X_returns_L);
+    tcase_add_test(shorthand, add_XC_plus_X_returns_C);
 
     suite_add_tcase(s, basic);
     suite_add_tcase(s, ordering);
