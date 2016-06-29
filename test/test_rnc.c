5cf5433c7b7f2a2b938c8b4d2edd2578fd43e773
Kristen Smith <ksmith2@pillartechnology.com>
2016-06-28 20:57:05 -0400

    add test for XX-II=XVIII for good measure

--- test/test_rnc.c
+++ test/test_rnc.c
@@ -564,6 +564,17 @@ START_TEST (subtract_MM_minus_D_returns_MD)
 }
 END_TEST
 
+START_TEST (subtract_XX_minus_II_returns_XVIII)
+{
+    int maxlen = 10;
+    char answer[maxlen];
+
+    subtract("XX", "II", answer, maxlen);
+
+    ck_assert_str_eq("XVIII", answer);
+}
+END_TEST
+
 Suite* subtraction_suite(void)
 {
     Suite *s;
@@ -591,6 +602,7 @@ Suite* subtraction_suite(void)
     tcase_add_test(borrow, subtract_CC_minus_L_returns_CL);
     tcase_add_test(borrow, subtract_DD_minus_CC_returns_DCCC);
     tcase_add_test(borrow, subtract_MM_minus_D_returns_MD);
+    tcase_add_test(borrow, subtract_XX_minus_II_returns_XVIII);
 
     suite_add_tcase(s, basic);
     suite_add_tcase(s, expand);
